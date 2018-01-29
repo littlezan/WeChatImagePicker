345ad1b8cc2aa38091d9c8ecd4e84cd325f97127
pengzan <263643193@qq.com>
2018-01-29 20:43:01 +0800

    FileProvide 冲突

---
+++ imagepicker/src/main/java/com/littlezan/imagepicker/util/MyFileProvider.java
@@ -0,0 +1,14 @@
+package com.littlezan.imagepicker.util;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * ClassName: MyFileProvider
+ * Description:
+ *
+ * @author 彭赞
+ * @version 1.0
+ * @since 2018-01-29  20:41
+ */
+public class MyFileProvider extends FileProvider {
+}
